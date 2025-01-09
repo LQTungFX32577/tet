d3d83714614003fb6ab5d6fbcb5f897522e77e0a
LQTungFX32577 <159262917+LQTungFX32577@users.noreply.github.com>
2025-01-09 16:14:03 +0700

    Update script.js

--- script.js
+++ script.js
@@ -3,7 +3,7 @@ const slides = document.querySelectorAll('.banner-slide');
 const totalSlides = slides.length;
 
 function addToCart() {
-    location.assign("https://order.ipos.vn/menu?pos_parent=BRAND-D9A5&pos_id=97335&source=DEFAULT");
+    location.assign("https://order.ipos.vn/menu?pos_parent=BRAND-D9A5&pos_id=107563&source=DEFAULT");
   }
 
 function showNextSlide() {
